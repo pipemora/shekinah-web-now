6b20f8ee3b5265daca7477c0460ad9afe19537e0
pipemora <tarefasimple@gmail.com>
2016-12-25 19:42:21 -0500

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/history'
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root 'application#hello'
+  get  'static_pages/history'
+  root 'static_pages#home'
 end
